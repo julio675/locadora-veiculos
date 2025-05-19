c717f31c7511f5a448460de03c1abdbc63c69608
Tarcisio-Fontes <166028123+Tarcisio-Fontes@users.noreply.github.com>
2025-05-19 14:09:17 -0300

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -1,5 +1,44 @@
 package projeto;
 
-public class Moto {
+public class Moto extends Veiculo implements Automovel{
+	
+	private int cc;
+	private String tipo;
+	
+	public Moto(String modelo, String marca, int ano, double precoBase, int cc, String tipo) {
+		super(modelo,marca,ano,precoBase);
+		this.setCc(cc);
+		this.setTipo(tipo);
+	}
 
+	public int getCc() {
+		return cc;
+	}
+
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public double acelerar(double velocidade) {
+		return velocidade+20;
+	}
+
+	@Override
+	public double frear(double velocidade) {
+		return velocidade-20;
+	}
+
+	@Override
+	public boolean temMotor() {
+		return true;
+	}
 }
