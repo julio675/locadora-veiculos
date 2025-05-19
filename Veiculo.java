0cb989255d433aba5ca677e40e926b3084c887df
Tarcisio-Fontes <166028123+Tarcisio-Fontes@users.noreply.github.com>
2025-05-19 14:10:02 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,5 +1,49 @@
 package projeto;
 
-public class Veiculo {
+public abstract class Veiculo {
+	
+	private String modelo;
+	private String marca;
+	private int ano;
+	private double precoBase;
+	
+	public Veiculo(String modelo, String marca, int ano, double precoBase) {
+		super();
+		this.setModelo(modelo);
+		this.setMarca(marca);
+		this.setAno(ano);
+		this.setPrecoBase(precoBase);
+	}
 
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public double getPrecoBase() {
+		return precoBase;
+	}
+
+	public void setPrecoBase(double precoBase) {
+		this.precoBase = precoBase;
+	}
 }
