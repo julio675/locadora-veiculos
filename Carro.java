ac6229aa6f526abafa7c63f07bfa68248c0a34c8
Júlio César <163342773+julio675@users.noreply.github.com>
2025-05-21 18:53:00 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -2,14 +2,36 @@ package projeto;
 import java.util.Scanner;
 
 public class Carro extends Veiculo implements Automovel{
+	
 	private int qtdPortas;
 	private boolean conversivel;
+	private String tipoCombustivel;
+	
 	private Scanner leitor = new Scanner(System.in);
 
-	public Carro(String modelo, String marca, int ano, double precoBase, int qtdPortas, boolean conversivel) {
+	public Carro(String modelo, String marca, int ano, double precoBase, int qtdPortas, boolean conversivel, String tipoCombustivel) {
 		super(modelo, marca, ano, precoBase);
 		this.qtdPortas = qtdPortas;
 		this.conversivel = conversivel;
+		this.tipoCombustivel = tipoCombustivel;
+	}
+
+	
+	public int getQtdPortas() {
+		return qtdPortas;
+	}
+
+
+	public boolean isConversivel() {
+		return conversivel;
+	}
+
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
+
+	public Scanner getLeitor() {
+		return leitor;
 	}
 
 	@Override
@@ -29,8 +51,8 @@ public class Carro extends Veiculo implements Automovel{
 
 	@Override
 	public String toString() {
-		return String.format("Modelo: %s\nMarca: %s\nAno: %d\nPreço: %.2f\nPortas: %d\nConversível: %b\n",
-			    getModelo(), getMarca(), getAno(), getPrecoBase(), qtdPortas, conversivel);
+		return String.format("===== Carro =====\n\nModelo: %s\nMarca: %s\nAno: %d\nPreço: %.4f\nPortas: %d\nConversível: %b\nCombustível: %s\n",
+			    getModelo(), getMarca(), getAno(), getPrecoBase(), qtdPortas, conversivel, tipoCombustivel);
 
 	}	
 	
