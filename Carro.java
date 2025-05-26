42421be8fbe34914aa87193386c74013e0cc9c10
Júlio César <163342773+julio675@users.noreply.github.com>
2025-05-26 08:36:23 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,5 +1,55 @@
 package projeto;
+import java.util.Scanner;
 
-public class Carro {
+public class Carro extends Veiculo implements Automovel{
+	
+	private int qtdPortas;
+	private boolean conversivel;
+	private String tipoCombustivel;
+	
+	private Scanner leitor = new Scanner(System.in);
 
+	public Carro(String modelo, String marca, int ano, double precoBase, int qtdPortas, boolean conversivel, String tipoCombustivel) {
+		super(modelo, marca, ano, precoBase);
+		this.qtdPortas = qtdPortas;
+		this.conversivel = conversivel;
+		this.tipoCombustivel = tipoCombustivel;
+	}
+
+	
+	public int getQtdPortas() {
+		return qtdPortas;
+	}
+
+
+	public boolean isConversivel() {
+		return conversivel;
+	}
+
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
+
+	@Override
+	public double acelerar(double velocidade) {
+		return velocidade + 20;
+	}
+
+	@Override
+	public double frear(double velocidade) {
+		return velocidade - 20;
+	}
+
+	@Override
+	public boolean temMotor() {
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("===== Carro =====\n\nModelo: %s\nMarca: %s\nAno: %d\nPreço: %.4f\nPortas: %d\nConversível: %b\nCombustível: %s\n",
+			    getModelo(), getMarca(), getAno(), getPrecoBase(), qtdPortas, conversivel, tipoCombustivel);
+
+	}	
+	
 }
