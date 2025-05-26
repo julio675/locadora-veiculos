70e0881c7db8a593ca531f5017432b46349ac364
Júlio César <163342773+julio675@users.noreply.github.com>
2025-05-26 08:34:47 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -2,11 +2,9 @@ package locadora_veiculos;
 
 public class Carro extends Veiculo implements Automovel {
 	
-	final String veiculoTipo = "moto";
-	
 	public Carro(String modelo, String marca, int ano, double precoBase) {
 		super(modelo,marca,ano,precoBase);
-		setVeiculoTipo("carro");
+		
 	}
 	
 	public double acelerar(double velocidade) {
